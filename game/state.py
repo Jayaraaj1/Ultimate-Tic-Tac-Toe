a22d7efec50ff498153f7357bcd24068ff062adc
Jayaraaj1 <kadiyala.jayaraaj@gmail.com>
2026-09-03 18:04:24 +0100

    Added local board draw and win detection

--- game/state.py
+++ game/state.py
@@ -2,6 +2,7 @@ empty = ongoing = 0
 X = 1
 O = -1
 draw = 2
+winning_lines = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
 
 class GameState():
     def __init__(self):
@@ -23,12 +24,22 @@ class GameState():
         if(not(self.is_valid_move(board_index, cell_index))):
             return False
         self.board[board_index][cell_index] = self.current_player
+        self.local_status[board_index] = self.check_local_board(board_index)
         if(self.local_status[cell_index] == ongoing):
             self.active_board = cell_index
         else:
             self.active_board = None
         self.current_player = -self.current_player
         return True
+    
+    def check_local_board(self, board_index):
+        for line in winning_lines:
+            a, b, c = line
+            if((self.board[board_index][a] != empty) and (self.board[board_index][a] == self.board[board_index][b] == self.board[board_index][c])):
+                return self.board[board_index][a]
+        if empty not in self.board[board_index]:
+            return draw
+        return ongoing
             
 
 state = GameState()
